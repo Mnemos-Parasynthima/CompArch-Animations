6ad124ba1a6c8230dcdd297d4e4dcfcaa24642d3
Jorge <jorgegarcia_tx@outlook.com>
2025-02-08 18:17:23 -0600

    Complete signed addition

--- integers/Addition.py
+++ integers/Addition.py
@@ -91,9 +91,9 @@ class UnsignedAddition(Scene):
 
 class SignedAddition(Scene):
 	def construct(self):
-		nbits = 5
-		numX = 2
-		numY = -3
+		nbits = 3
+		numX = 3
+		numY = 3
 		mathSum = numX + numY
 		# Sum for signed addition is more complex, with cases depending on n
 		lowerBound = -2**(nbits-1)
@@ -105,122 +105,134 @@ class SignedAddition(Scene):
 
 		wheel = NumberWheel(nbits, signed=True)
 
-		# title = Text("Signed Addition").to_edge(UP)
-		# self.play(FadeIn(title))
+		title = Text("Signed Addition").to_edge(UP)
+		self.play(FadeIn(title))
 
-		# uAddEqu = MathTex("x \oplus_{s}^{n} y").next_to(title, RIGHT, 1)
-		# self.play(FadeIn(uAddEqu))
+		uAddEqu = MathTex("x \oplus_{s}^{n} y").next_to(title, RIGHT, 1)
+		self.play(FadeIn(uAddEqu))
 
 		n = MathTex(f"n = {nbits}").to_corner(UL).shift(DOWN)
 		x = MathTex(f"x = {numX}").next_to(n, DOWN)
 		y = MathTex(f"y = {numY}").next_to(x, DOWN)
-		# self.play(FadeIn(n), FadeIn(x), FadeIn(y))
+		self.play(FadeIn(n), FadeIn(x), FadeIn(y))
 
-		# self.wait(0.5)
+		self.wait(0.5)
 
 		self.play(FadeIn(wheel))
 		
-		# self.wait(0.25)
+		self.wait(0.25)
 
 		# When x is negative, highlight going CCW from 0 until it hits the appropriate num
 		# When x is positive, do same as in Unsigned
-		# self.play(x.animate.set_color(YELLOW))
-		# # print("numX: ", numX)
-		if numX >= 0:
-			for xi in range(numX+1):
-				print(xi)
-		# 		self.play(wheel.highlightNumber(xi, YELLOW, False), run_time=0.4)
-		# 		self.play(wheel.dehighlightNumber(xi, False), run_time=0.3)
-		else:
-			for xi in range(-1, numX-1, -1):
-				print(xi)
-		# 		# print(xi)
-		# 		self.play(wheel.highlightNumber(xi, YELLOW, False), run_time=0.4)
-		# 		self.play(wheel.dehighlightNumber(xi, False), run_time=0.3)
-		# self.play(x.animate.set_color(WHITE))
+		self.play(x.animate.set_color(YELLOW))
 
-		# self.play(y.animate.set_color(GREEN))
+		rangeIter:range = None
+		if numX >= 0: rangeIter = range(numX+1)
+		else: rangeIter = range(-1, numX-1, -1)
+
+		for xi in rangeIter:
+			self.play(wheel.highlightNumber(xi, YELLOW, False), run_time=0.4)
+			self.play(wheel.dehighlightNumber(xi, False), run_time=0.3)
+		self.play(x.animate.set_color(WHITE))
+
+		self.play(y.animate.set_color(GREEN))
 
 		if numY >= 0 and numX >= 0: xi += 1
 		elif numY >= 0 and numX < 0: xi += 1
 		elif numY < 0 and numX >= 0: xi -= 1
 		else: xi -= 1
+		# xi is used to index from the right
 
 		print("xi: ", xi)
 		self.play(FadeIn(wheel.setupArrow(xi)))
 		# print("numY: ", numY)
 		if numY >= 0:
 			for yi in range(numY):
-				# print(yi, xi + yi)
 				anims:list[Animation|AnimationGroup|VMobject] = []
 
-				anims.append(wheel.highlightNumber(xi + yi, ORANGE, False))
-				# self.play(anim, run_time=0.4)
-				# if xi+yi > wheel.totalSlices - 1:
-				# 	mathNum, animMathNum = wheel.highlightMathNumber(xi + yi, GREEN)
-					# if yi != numY-1: anims.append(Rotate(wheel.arrow, -PI/4, about_point=ORIGIN, rate_func=rate_functions.ease_in_expo))
-				# 	anims.append(animMathNum)
-					# self.play(animMathNum, run_time=1)
+				if yi != numY-1: anims.append(wheel.rotateArrow())
+
+				blink = False
+				if xi+yi >= wheel.totalSlices/2:
+					if not wheel.flag: wheel.flag = True
+					mathNum, animMathNum = wheel.highlightMathNumber(xi + yi, GREEN)
+					anims.append(animMathNum)
+					blink = True
+
+				anims.append(wheel.highlightNumber(xi + yi, ORANGE, blink))
 				self.play(*anims, run_time=0.4)
 
-				self.play(wheel.dehighlightNumber(xi + yi, False), run_time=0.3)
-				# if xi+yi > wheel.totalSlices - 1: self.play(wheel.dehighlightMathNumber(mathNum, xi + yi), run_time=0.3)
+				self.play(wheel.dehighlightNumber(xi + yi, blink), run_time=0.3)
+				if wheel.flag: self.play(wheel.dehighlightMathNumber(mathNum, xi + yi), run_time=0.3)
 		else:
 			# numY being negative would mean for the indices to be decrementing
 			# Since the real indices is taken care of in highlightNumber(), the pseudo
 			
 			for yi in range(numY, 0):
+				# yi is used to simply as a counting method, doing range(abs(numY)) would still work
+
 				numberText = int(wheel.getNumber(xi).text)
 				numberText = wheel._toUnsigned(numberText) if numberText < 0 else numberText
-				xi -= 1
 
 				anims:list[Text|Succession|Rotate] = []
-				anims.append(wheel.highlightNumber(numberText, ORANGE, False))
-
 				if yi != -1: anims.append(wheel.rotateArrow(False))
+
+				blink = False
+				print(xi, yi)
+				if abs(xi) > wheel.totalSlices/2:
+					if not wheel.flag: wheel.flag = True
+					# print("Overflow")
+					mathNum, animMathNum = wheel.highlightMathNumber(xi, GREEN)
+					anims.append(animMathNum)
+					blink = True
+
+				anims.append(wheel.highlightNumber(numberText, ORANGE, blink))
 				self.play(*anims, run_time=0.4)
-				
-				self.play(wheel.dehighlightNumber(numberText, False), run_time=0.3)
 
-		# self.play(x.animate.set_color(WHITE))
+				self.play(wheel.dehighlightNumber(numberText, blink), run_time=0.3)
+				if wheel.flag: self.play(wheel.dehighlightMathNumber(mathNum, xi), run_time=0.3)
 
-		# self.wait(0.5)
+				xi -= 1
 
-		# res = MathTex(f"{_sum}").to_edge(DOWN)
+		self.play(x.animate.set_color(WHITE))
 
-		# mathSumText = MathTex(f"s = {numX} + {numY}").next_to(y, DOWN * 1).shift(RIGHT * 0.5)
-		# self.play(FadeIn(mathSumText))
+		self.wait(0.5)
 
-		# equ:str = None
-		# equstr0:str = None
-		# equstr1:str = None
-		# endequstr = str(_sum) + " = " + str(_sum)
+		res = MathTex(f"{_sum}").to_edge(DOWN)
 
-		# if mathSum >= upperBound:
-		# 	equ = MathTex(" = s - 2^n").next_to(res)
-		# 	equstr0 = str(_sum) + " = " + str(mathSum) + " - 2^" + str(nbits)
-		# 	equstr1 = str(_sum) + " = " + str(mathSum) + " - " + str(2**nbits)
-		# elif mathSum < lowerBound:
-		# 	equ = MathTex(" = s + 2^n").next_to(res)
-		# 	equstr0 = str(_sum) + " = " + str(mathSum) + " + 2^" + str(nbits)
-		# 	equstr1 = str(_sum) + " = " + str(mathSum) + " + " + str(2**nbits)
-		# else:
-		# 	equ = MathTex(" = s").next_to(res)
+		mathSumText = MathTex(f"s = {numX} + {numY}").next_to(y, DOWN * 1).shift(RIGHT * 0.5)
+		self.play(FadeIn(mathSumText))
 
-		# self.play(TransformFromCopy(wheel.getNumber(_sum), res))
+		equ:str = None
+		equstr0:str = None
+		equstr1:str = None
+		endequstr = str(_sum) + " = " + str(_sum)
 
-		# requ = VGroup(res, equ).arrange(RIGHT).to_edge(DOWN)
-		# self.play(FadeIn(requ))
+		if mathSum >= upperBound:
+			equ = MathTex(" = s - 2^n").next_to(res)
+			equstr0 = str(_sum) + " = " + str(mathSum) + " - 2^" + str(nbits)
+			equstr1 = str(_sum) + " = " + str(mathSum) + " - " + str(2**nbits)
+		elif mathSum < lowerBound:
+			equ = MathTex(" = s + 2^n").next_to(res)
+			equstr0 = str(_sum) + " = " + str(mathSum) + " + 2^" + str(nbits)
+			equstr1 = str(_sum) + " = " + str(mathSum) + " + " + str(2**nbits)
+		else:
+			equ = MathTex(" = s").next_to(res)
 
-		# if equstr0:	
-		# 	equ1 = MathTex(equstr0).to_edge(DOWN)
-		# 	self.play(Transform(requ, equ1, replace_mobject_with_target_in_scene=True))
+		self.play(TransformFromCopy(wheel.getNumber(_sum), res))
 
-		# 	equ2 = MathTex(equstr1).to_edge(DOWN)
-		# 	self.play(Transform(equ1, equ2, replace_mobject_with_target_in_scene=True))
+		requ = VGroup(res, equ).arrange(RIGHT).to_edge(DOWN)
+		self.play(FadeIn(requ))
+
+		if equstr0:
+			equ1 = MathTex(equstr0).to_edge(DOWN)
+			self.play(Transform(requ, equ1, replace_mobject_with_target_in_scene=True))
+
+			equ2 = MathTex(equstr1).to_edge(DOWN)
+			self.play(Transform(equ1, equ2, replace_mobject_with_target_in_scene=True))
 		
-		# equend = MathTex(endequstr).to_edge(DOWN)
-		# self.play(Transform((requ if not None else equ2), equend, replace_mobject_with_target_in_scene=True))
+		equend = MathTex(endequstr).to_edge(DOWN)
+		self.play(Transform((requ if not equ2 else equ2), equend, replace_mobject_with_target_in_scene=True))
 
 
 		self.wait(2)
