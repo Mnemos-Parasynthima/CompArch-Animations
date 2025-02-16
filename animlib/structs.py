e354d2a344ca7aa0b5c3c45e37ac2ad3d1a99bf8
Jorge <jorgegarcia_tx@outlook.com>
2025-02-15 22:24:22 -0600

    Restructured

--- animlib/structs.py
+++ animlib/structs.py
@@ -2,68 +2,41 @@ from manim import VGroup, Text, DOWN, LEFT, RIGHT, ManimColor, Table
 from .types import Type, TypeEnum
 
 from typing_extensions import Self
+from abc import ABC, abstractmethod
 
-class Struct(VGroup):
-	def __init__(self, name:str, objs:list[Type], fontSize=14):
-		assert(len(objs) != 0)
 
+class DerivedType(VGroup, ABC):
+	def __init__(self, objs:list[type]):
 		super().__init__()
 
-		self.structName = name
 		self.objs = objs
-		self.size = len(objs)
-		self.alignBy = max(prop.sizeof() for prop in self.objs)
+		self.objsLen = len(objs)
 		self.paddings:list[int] = []
 
-		self.add(Text(f"struct {name} {{", font_size=fontSize))
-
-		for obj in self.objs:
-			self.add(obj)
-			
-
-		self.add(Text("};", font_size=fontSize))
-
-		self.arrange(DOWN, aligned_edge=LEFT)
-
-		for i in range(1, len(objs)+1):
-			self.submobjects[i].shift(RIGHT * 0.2)
-
-		# print(type(struct), type(self))
-
-		# print(len(self.submobjects))
-
 	def __len__(self):
-		return self.size
+		return self.objsLen
 
 	def __getitem__(self, index:int) -> Type:
 		return self.submobjects[index+1]
+	
+	@abstractmethod
+	def sizeof(self) -> int:
+		'''
+		Returns the number of bytes the structure occupies, including padding.
+		'''
+		pass
+
+	@abstractmethod
+	def swap(self, propIdx1:int, propIdx2:int) -> Self:
+		'''
+		Swaps two given properties. This might affect padding.
+		'''
+		pass
 
 	def getAlignBy(self) -> int:
 		return self.alignBy
 
-	def getSize(self) -> int:
-		'''
-		Returns the number of bytes the structure occupies, including padding.
-		'''
-		offset = 0
-
-		for prop in self.objs:
-			size = prop.sizeof()
-			align = size
-
-			padding = (align - (offset % align)) % align
-			offset += padding + size
-
-			self.paddings.append(padding)
-
-		finalPadding = (self.alignBy - (offset % self.alignBy)) % self.alignBy
-		self.paddings.append(finalPadding)
-		# There will always be a padding of 0 at the beginning
-		self.paddings.remove(0)
-		
-		return offset + finalPadding
-
-	def sizeof(self, index:int) -> tuple[int, int]:
+	def propSizeof(self, index:int) -> tuple[int, int]:
 		'''
 		Returns the size of bytes of the property indicated by the given index, as well as the padding it requires.
 		'''
@@ -84,25 +57,99 @@ class Struct(VGroup):
 		originalColor = _type._color
 
 		return _type.animate.set_color(originalColor)
+
+class Struct(DerivedType):
+	def __init__(self, name:str, objs:list[Type], fontSize=14):
+		assert(len(objs) != 0)
+
+		super().__init__(objs)
+
+		self.structName = name
+
+		self.add(Text(f"struct {name} {{", font_size=fontSize))
+
+		# To reduce the constant for O(n), do the max alignBy, adding, and padding at the same time
+		maxAlign:int = 0
+		offset = 0
+		for obj in self.objs:
+			self.add(obj)
+
+			objSizeof = obj.sizeof()
+			if objSizeof > maxAlign: maxAlign = objSizeof
+
+			align = objSizeof
+
+			padding = (align - (offset % align)) % align
+			offset += padding + objSizeof
+
+			self.paddings.append(padding)
+
+		self.alignBy = maxAlign
+		finalPadding = (self.alignBy - (offset % self.alignBy)) % self.alignBy
+		self.paddings.append(finalPadding)
+		# There will always be a padding of 0 at the beginning
+		self.paddings.remove(0)
+		self._sizeof = offset + finalPadding
+		
+		self.add(Text("};", font_size=fontSize))
+
+		self.arrange(DOWN, aligned_edge=LEFT)
+
+		for i in range(1, len(objs)+1):
+			self.submobjects[i].shift(RIGHT * 0.2)
+
+	def sizeof(self) -> int:
+		return self._sizeof
 	
-	def swap(self, propIdx1:int, propIdx2:int) -> Self:
-		'''
-		Swaps two given properties. This might affect padding.
-		'''
+	def swap(self, propIdx1, propIdx2):
 		pass
 
-class Union(VGroup):
-	def __init__(self):
-		super().__init__()
+class Union_(DerivedType):
+	def __init__(self, name:str, objs:list[Type], fontSize=14):
+		assert(len(objs) != 0)
+
+		super().__init__(objs)
+
+		self.unionName = name
+
+		self.add(Text(f"union {name} {{", font_size=fontSize))
+		
+		maxAlign:int = 0
+		for obj in self.objs:
+			self.add(obj)
+
+			objSizeof = obj.sizeof()
+			if objSizeof > maxAlign: maxAlign = objSizeof
+
+		self.alignBy = maxAlign
+
+		# Another pass is needed since the max alignment needs to be known
+		for obj in self.objs:
+			objSizeof = obj.sizeof()
+			padding = (maxAlign - objSizeof) % maxAlign
+			self.paddings.append(padding)
+	
+		self.add(Text("};", font_size=fontSize))
+
+		self.arrange(DOWN, aligned_edge=LEFT)
+
+		for i in range(1, len(objs)+1):
+			self.submobjects[i].shift(RIGHT * 0.2)
+
+	def sizeof(self) -> int:
+		return self.alignBy
+	
+	def swap(self, propIdx1, propIdx2):
+		pass
 
 class StructTable(Table):
 	def __init__(self, struct:Struct):
-		self.rows = struct.size + len(struct.paddings) + 1
+		self.rows = struct.objsLen + len(struct.paddings) + 1
 
 		table:list[list[str]] = []
 
 		# print(struct.size, len(struct.paddings))
-		for m, p in zip(range(struct.size), range(len(struct.paddings))):
+		for m, p in zip(range(len(struct)), range(len(struct.paddings))):
 			prop = struct[m]
 			pad:int = struct.paddings[p]
 
