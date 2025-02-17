210180ae5bd2d691e6ed8e212be88fb677b84d1e
Jorge <jorgegarcia_tx@outlook.com>
2025-02-17 10:36:00 -0600

    Interactiveness

--- integers/IntegersScene.py
+++ integers/IntegersScene.py
@@ -8,23 +8,42 @@ from animlib.numwheel import NumberWheel
 
 from manim import Scene, MathTex, UL, DOWN
 
+NBITS_LIMIT = 5 # Figure out what is an appropriate limit
+
 class IntegersScene(Scene):
-	def __init__(self, nbits:int, numX:int, numY:int):
+	def __init__(self):
 		super().__init__()
 		# self.renderer.window = window
-		self.nbits:int = nbits
-		self.numX = numX
-		self.numY = numY
-		self._sum:int = (numX + numY) % 2**nbits
+		self.nbits = 0
+		self.numX = 0
+		self.numY = 0
+		self._sum = 0
 
-		self.n = MathTex(f"n = {nbits}").to_corner(UL).shift(DOWN)
-		self.x = MathTex(f"x = {numX}").next_to(self.n, DOWN)
-		self.y = MathTex(f"y = {numY}").next_to(self.x, DOWN)
+		self.n:MathTex = None
+		self.x:MathTex = None
+		self.y:MathTex = None
 
-		self.wheel = NumberWheel(nbits)
+		self.wheel:NumberWheel = None
 
 	def init(self):
-		pass
+		nbits = int(input("How many bits? "))
+
+		# Since more bits means more sectors/wedges/etc, it means more costly and slower
+		# Limit input to a low number
+		while nbits <= 0 or nbits > NBITS_LIMIT:
+			print("N bits cannot be negative or more than ", NBITS_LIMIT)
+			nbits = int(input("How many bits? "))
+
+		self.nbits = nbits
+		self.wheel = NumberWheel(nbits)
+
+		# Regarding numX and numY, it varies whether it is unsigned or signed
+		# That should be checked for each child class
+
+	def endInit(self):
+		self.n = MathTex(f"n = {self.nbits}").to_corner(UL).shift(DOWN)
+		self.x = MathTex(f"x = {self.numX}").next_to(self.n, DOWN)
+		self.y = MathTex(f"y = {self.numY}").next_to(self.x, DOWN)
 
 	def view(self):
 		outfile:Path = self.renderer.file_writer.movie_file_path
