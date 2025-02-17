bfb6c60e792165bbed3d4d76ceedcf4aa030f22c
Jorge <jorgegarcia_tx@outlook.com>
2025-02-16 21:41:38 -0600

    Showing labels and dehighlighting

--- animlib/mem.py
+++ animlib/mem.py
@@ -2,17 +2,21 @@ from manim import VGroup, Rectangle, RIGHT, DOWN, AnimationGroup, UP, Transform,
 from manim.opengl import OpenGLVGroup
 from numpy import array_equal
 from .hexdec import Hexadecimal
+from .funcs import inttstr
 
 
-class MemoryBlock(OpenGLVGroup):
+class MemoryBlock(VGroup):
 	HORIZONTAL = 0
 	VERTICAL = 1
 
-	def __init__(self, numBlocks=2, layout=HORIZONTAL, startAddr=Hexadecimal("0x0"), endAddr=Hexadecimal("0x1"), **kwargs):
+	def __init__(self, numBlocks=2, layout=HORIZONTAL, startAddr=Hexadecimal("0x0"), endAddr=Hexadecimal("0x1"), scale:int=-1, start:int=0x0, end:int=0x1, **kwargs):
 		super().__init__(**kwargs)
 
+		self.start = start
+		self.end = end
+
 		maxBlocks = 10
-		scale = min(1, maxBlocks / numBlocks)
+		if scale == -1: scale = min(1, maxBlocks / numBlocks)
 		blockSize = 0.75 * scale
 
 		aspectRatio = 1 if layout == self.HORIZONTAL else 1.5
@@ -27,11 +31,13 @@ class MemoryBlock(OpenGLVGroup):
 		Order of the exact data depends on the order of this.setByte()
 		'''
 
-		self.blocks = OpenGLVGroup(*[
+		self.blocks = VGroup(*[
 			Rectangle(width=blockSize*aspectRatio,height=blockSize) if layout==self.VERTICAL
 			else Rectangle(width=blockSize,height=blockSize) for _ in range(numBlocks)
 		])
 		self.layout = layout
+
+		# byteData will hold the organized data bytes based on a provided index
 		self.byteData:list[Hexadecimal] = [None] * numBlocks
 
 		if layout == self.HORIZONTAL:
@@ -43,11 +49,13 @@ class MemoryBlock(OpenGLVGroup):
 		
 		self.add(self.blocks)
 
-		textScale = scale * 0.75
-		startLabel = startAddr.scale(textScale).next_to(self.blocks[0], labelDir, buff=0.1)
+		self.textScale = scale * 0.75
+		startLabel = startAddr.scale(self.textScale).next_to(self.blocks[0], labelDir, buff=0.1)
 		self.add(startLabel)
 
-		endLabel = endAddr.scale(textScale).next_to(self.blocks[-1], labelDir, buff=0.1)
+		self.labels:list[Hexadecimal] = []
+
+		endLabel = endAddr.scale(self.textScale).next_to(self.blocks[-1], labelDir, buff=0.1)
 		self.add(endLabel)
 
 	def transpose(self):
@@ -121,21 +129,43 @@ class MemoryBlock(OpenGLVGroup):
 		
 		return AnimationGroup(*animations)
 
+	def showLabel(self, index:int) -> Hexadecimal:
+		label = Hexadecimal(inttstr(self.start + index)).scale(self.textScale).next_to(self.blocks[index], RIGHT, buff=0.1)
+		self.add(label)
+		self.labels.append(label)
+		return label.animate.fade(0).build()
+
+	# def hideLabel(self, index:int) -> Hexadecimal:
+
+
 	def setByte(self, index:int, data:Hexadecimal) -> Hexadecimal:
 		self.add(data)
 		self.byteData[index] = data
+		# data exists in two places:
+		# 	byteData: the organized array of bytes (to show proper order)
+		# 	self.submobjects: the actual vgroup itself
 
 		return data.animate.move_to(self.blocks[index].get_center())
 
 	def clearByte(self, index:int):
+		byte = self.byteData[index]
 
-		pass
+		# To clear a byte, it means to remove it from byteData and from the vgroup itself
+		self.remove(byte)
+		self.byteData[index] = None
+		
+		return byte.animate.fade(1).build()
 
-	def highlightByte(self, index:int, color:str):
+	def highlightByte(self, index:int, color:str) -> Rectangle:
 		return self.blocks[index].animate.set_color(color)
 	
-	def dehighlightByte(self, index:int):
+	def dehighlightByte(self, index:int) -> Rectangle:
 		return self.blocks[index].animate.set_color("#ffffff")
 	
+	def dehighlightBytes(self) -> AnimationGroup:
+		anims:list[Rectangle] = []
 
+		for i in range(len(self.blocks)):
+			anims.append(self.dehighlightByte(i))
 
+		return AnimationGroup(*anims)
