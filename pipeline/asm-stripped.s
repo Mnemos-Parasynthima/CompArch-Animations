9caaaebcf63ba07b501388bd7f3a14e072a71d4a
Jorge <jorgegarcia_tx@outlook.com>
2025-03-11 19:37:59 -0500

    Assembly sample

---
+++ pipeline/asm-stripped.s
@@ -0,0 +1,8 @@
+start:
+  add x0, x0, #1
+  add x1, x1, #0xfff
+  nop
+	nop
+	nop
+	add x4, x0, #1
+  ret
---
+++ pipeline/asm.s
@@ -0,0 +1,13 @@
+	.arch armv8-a
+	.text
+	.align 2
+	.p2align 3,,7
+start:
+  add x0, x0, #1
+  add x1, x1, #0xfff
+  nop
+	nop
+	nop
+	add x4, x0, #1
+  ret
+	.size start, .-start
