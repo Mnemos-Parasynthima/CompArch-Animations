702f938eba2a6541c9c5f878e8e7d8430eaac31a
Jorge <jorgegarcia_tx@outlook.com>
2025-03-11 19:35:59 -0500

    Renamed and made distinct class for using verbatic for code and not as hex

--- animlib/hexdec.py
+++ animlib/hexdec.py
@@ -1,12 +1,26 @@
-from manim import VGroup, Tex, TexTemplate
+from manim import VGroup, Tex, TexTemplate, VMobject
 from manim.opengl import OpenGLVGroup
 
-class Hexadecimal(VGroup):
+
+class Verbatim(VMobject):
 	def __init__(self, value:str, color:str = None, fontSize:int = 48, **kwargs):
 		super().__init__(**kwargs)
 
 		self.value = value
-		self.numval = -1 if (value == "0x" or value == "0b") else int(value, base=16)
-		num = Tex(f"\\verb|{value}|", font_size=fontSize, color=color)
+		
+		verb = Tex(f"\\verb|{value}|", font_size=fontSize, color=color)
+		self.add(verb)
 
-		self.add(num)
+
+class Hexadecimal(Verbatim):
+	def __init__(self, value, color = None, fontSize = 48, **kwargs):
+		super().__init__(value, color, fontSize, **kwargs)
+
+		self.numval = -1 if (value == "0x" or value == "0b") else int(value, base=16)
+
+# Doing `Verbatim()` for text to be seen as code is too not clear
+# Maybe there is a better way to create a different name/alias
+
+class CodeBlock(Verbatim):
+	def __init__(self, value, color = None, fontSize = 48, **kwargs):
+		super().__init__(value, color, fontSize, **kwargs)
