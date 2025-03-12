bc91d6b377798f7b7ba952dc33aab5b8e70b41cf
Jorge <jorgegarcia_tx@outlook.com>
2025-03-12 10:51:33 -0500

    Updated to use CodeBlock

--- animlib/pipeline/IMem.py
+++ animlib/pipeline/IMem.py
@@ -1,10 +1,10 @@
-from manim import VGroup, Rectangle, Text, Arrow, Tex, MathTex, RIGHT, DOWN, UP, LEFT, Dot, RED, BLUE
+from manim import VGroup, Rectangle, Text, Arrow, RIGHT, DOWN, UP, LEFT
 
-from ..hexdec import Hexadecimal
+from ..hexdec import Hexadecimal, CodeBlock
 
 
 class IMem(VGroup):
-	def __init__(self, addr:int, errval:int, rval:int, imemWidth:float=1.5, imemHeight:float=2):
+	def __init__(self, imemWidth:float=1.5, imemHeight:float=2):
 		super().__init__()
 
 		self.imem = Rectangle(height=imemHeight, width=imemWidth)
@@ -15,30 +15,28 @@ class IMem(VGroup):
 		self.addrArrow = Arrow(
 			max_tip_length_to_length_ratio=0.15
 		).put_start_and_end_on(start=self.imem.get_left()+LEFT, end=self.imem.get_left())
-		self.addrLabel = Tex("\\verb|imem_addr|", font_size=26).next_to(self.addrArrow, DOWN, buff=0.1).shift(LEFT*0.2)
-		self.addrBits = MathTex("64", font_size=35).next_to(self.addrArrow, UP, buff=0.025)
-		self.addr = addr
-		self.addrText = Hexadecimal(hex(0), fontSize=40).next_to(self.addrLabel, DOWN, buff=0.1)
+		self.addrLabel = CodeBlock("imem_addr", fontSize=26).next_to(self.addrArrow, DOWN, buff=0.1).shift(LEFT*0.2)
+		self.addr = -1
+		self.addrText:Hexadecimal = None
 
-		addrGroup = VGroup(self.addrArrow, self.addrLabel, self.addrBits, self.addrText)
+		addrGroup = VGroup(self.addrArrow, self.addrLabel)
 
 		self.errArrow = Arrow(
 			max_tip_length_to_length_ratio=0.15
 		).put_start_and_end_on(start=self.imem.get_right()+(UP*0.5), end=self.imem.get_right()+RIGHT+(UP*0.5))
-		self.errLabel = Tex("\\verb|imem_err|", font_size=26).next_to(self.errArrow, UP, buff=0.1).shift(RIGHT*0.1)
-		self.err = errval
-		self.errText = Hexadecimal(hex(0), fontSize=40).next_to(self.errLabel, UP, buff=0.1)
+		self.errLabel = CodeBlock("imem_err", fontSize=26).next_to(self.errArrow, UP, buff=0.1).shift(RIGHT*0.1)
+		self.err = -1
+		self.errText:Hexadecimal = None
 
-		errGroup = VGroup(self.errArrow, self.errLabel, self.errText)
+		errGroup = VGroup(self.errArrow, self.errLabel)
 
 		self.rvalArrow = Arrow(
 			max_tip_length_to_length_ratio=0.15
 		).put_start_and_end_on(start=self.imem.get_right()+(DOWN*0.5), end=self.imem.get_right()+RIGHT+(DOWN*0.5))
-		self.rvalLabel = Tex("\\verb|imem_rval|", font_size=26).next_to(self.rvalArrow, DOWN, buff=0.1).shift(RIGHT*0.2)
-		self.rvalBits = MathTex("32", font_size=35).next_to(self.rvalArrow, UP, buff=0.025)
-		self.rval = rval
-		self.rvalText = Hexadecimal(hex(0), fontSize=40).next_to(self.rvalLabel, DOWN, buff=0.1)
+		self.rvalLabel = CodeBlock("imem_rval", fontSize=26).next_to(self.rvalArrow, DOWN, buff=0.1).shift(RIGHT*0.2)
+		self.rval = -1
+		self.rvalText:Hexadecimal = None
 
-		rvalGroup = VGroup(self.rvalArrow, self.rvalLabel, self.rvalBits, self.rvalText)
+		rvalGroup = VGroup(self.rvalArrow, self.rvalLabel)
 
 		self.add(*imemGroup, *addrGroup, *errGroup, *rvalGroup)
--- animlib/pipeline/PC.py
+++ animlib/pipeline/PC.py
@@ -1,32 +1,30 @@
-from manim import VGroup, Rectangle, Text, Arrow, Tex, MathTex, RIGHT, DOWN, UP, LEFT
+from manim import VGroup, Rectangle, Text, Arrow, RIGHT, DOWN, LEFT
 
-from ..hexdec import Hexadecimal
+from ..hexdec import Hexadecimal, CodeBlock
 
 
 class PC(VGroup):
-	def __init__(self, pcWidth:float, pcHeight:float, currPC:int, nextPC:int):
+	def __init__(self, pcWidth:float=1, pcHeight:float=3):
 		super().__init__()
 
-		self.pc = Rectangle(height=3, width=1)
+		self.pc = Rectangle(height=pcHeight, width=pcWidth)
 		self.pcLabel = Text("PC").move_to(self.pc.get_center())
 
 		pcGroup = VGroup(self.pc, self.pcLabel)
 
 		self.currPCArrow = Arrow(max_tip_length_to_length_ratio=0.15).put_start_and_end_on(start=self.pc.get_right(), end=self.pc.get_right() + RIGHT)
-		self.currPCLabel = Tex("\\verb|current_PC|", font_size=26).next_to(self.currPCArrow, DOWN).shift(RIGHT*0.3)
-		self.currPCBits = MathTex("64", font_size=35).next_to(self.currPCArrow, UP)
-		self.currPC = currPC
-		self.currPCText = Hexadecimal(hex(currPC), fontSize=40).next_to(self.currPCLabel, DOWN)
+		self.currPCLabel = CodeBlock("current_PC", fontSize=26).next_to(self.currPCArrow, DOWN).shift(RIGHT*0.3)
+		self.currPC = -1
+		self.currPCText:Hexadecimal = None
 
-		currPCGroup = VGroup(self.currPCArrow, self.currPCLabel, self.currPCBits, self.currPCText)
+		currPCGroup = VGroup(self.currPCArrow, self.currPCLabel)
 
 		self.nextPCArrow = Arrow(max_tip_length_to_length_ratio=0.15).put_start_and_end_on(start=self.pc.get_left()+LEFT, end=self.pc.get_left())
-		self.nextPCLabel = Tex("\\verb|next_PC|", font_size=26).next_to(self.nextPCArrow, DOWN).shift(LEFT*0.1)
-		self.nextPCBits = MathTex("64", font_size=35).next_to(self.nextPCArrow, UP)
-		self.nextPC = nextPC
-		self.nextPCText = Hexadecimal(hex(nextPC), fontSize=40).next_to(self.nextPCLabel, DOWN)
+		self.nextPCLabel = CodeBlock("next_PC", fontSize=26).next_to(self.nextPCArrow, DOWN).shift(LEFT*0.1)
+		self.nextPC = -1
+		self.nextPCText:Hexadecimal = None
 
-		nextPCGroup = VGroup(self.nextPCArrow, self.nextPCLabel, self.nextPCBits, self.nextPCText)
+		nextPCGroup = VGroup(self.nextPCArrow, self.nextPCLabel)
 
 		self.add(*pcGroup, *currPCGroup,*nextPCGroup)
 
