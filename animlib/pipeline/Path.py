11471a325235b1b641e2ec48aa593d2777049774
Jorge <jorgegarcia_tx@outlook.com>
2025-03-12 21:47:37 -0500

    Able to set intersection marks to a path

--- animlib/pipeline/Path.py
+++ animlib/pipeline/Path.py
@@ -1,10 +1,22 @@
-from manim import VMobject, ManimColor
-from manim.typing import Point3DLike_Array
+from manim import VMobject, ManimColor, Dot
+from manim.typing import Point3DLike_Array, Point3D
 
 
 class Path(VMobject):
+	'''
+	A path connecting two or more components.
+	'''
 	def __init__(self, *points:Point3DLike_Array, color:ManimColor, strokeWidth:float):
 		super().__init__()
-		self.points = points
+		self.pathPoints = points
 		self.set_points_as_corners(points)
 		self.set_stroke(color, strokeWidth)
+		self.intersections:list[Dot] = None
+
+	def markIntersections(self, indices:list[int], color:ManimColor):
+		selected:list[Point3D] = [self.pathPoints[i] for i in indices]
+
+		self.intersections = [Dot(point, color=color) for point in selected]
+		self.add(*self.intersections)
+
+		return self
