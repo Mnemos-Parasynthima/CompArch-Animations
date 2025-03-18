97d11d452ffeb1e5bb8376121afd65042acff1af
Jorge <jorgegarcia_tx@outlook.com>
2025-03-17 20:57:06 -0500

    Memory stage for pipeline complete

--- animlib/pipeline/Memory.py
+++ animlib/pipeline/Memory.py
@@ -1,6 +1,7 @@
-from manim import VGroup, RoundedRectangle, UP
+from manim import VGroup, UP, Rectangle, DL, RIGHT
 from .core import Stage, Register
 from .DMem import DMem
+from ..hexdec import CodeBlock
 
 
 class MemoryStage(Stage): 
@@ -23,4 +24,14 @@ class MemoryPipeline(Register):
 
 		self.add(*filter(None, self.components), *filter(None, self.componentsText))
 
-class MemoryElements(VGroup): pass
+class MemoryElements(Stage):
+	def __init__(self):
+		super().__init__(10, 3)
+
+		stageLabel = CodeBlock("memory_instr", fontSize=35).move_to(self.submobjects[0].get_corner(DL)).shift(UP*0.5 + RIGHT*1.3)
+		self.add(stageLabel)
+
+		self.dmem = Rectangle(width=2, height=1.5).shift(RIGHT*3.3 + UP*0.5)
+		self.dmemLabel = CodeBlock("dmem", fontSize=38).move_to(self.dmem.get_center())
+
+		self.add(self.dmem, self.dmemLabel)
