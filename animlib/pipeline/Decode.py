4e4a717a2c2482877e4c91169656e58d51c23047
Jorge <jorgegarcia_tx@outlook.com>
2025-03-17 20:20:26 -0500

    Decode stage for pipeline complete

--- animlib/pipeline/Decode.py
+++ animlib/pipeline/Decode.py
@@ -1,4 +1,4 @@
-from manim import VGroup, RoundedRectangle, LEFT, RIGHT, UP, DOWN, RED, BLUE, Text
+from manim import VGroup, RoundedRectangle, LEFT, RIGHT, UP, DOWN, RED, Text, Rectangle, DL, Arrow
 from .core import Stage, Register
 from .RegFile import RegFile
 from .logic import Mux
@@ -40,4 +40,85 @@ class DecodePipeline(Register):
 
 		self.add(*filter(None, self.components), *filter(None, self.componentsText))
 
-class DecodeElements(VGroup): pass
+
+class DecodeElements(Stage): 
+	def __init__(self):
+		super().__init__(10, 4)
+
+		stageLabel = CodeBlock("decode_instr", fontSize=35).move_to(self.submobjects[0].get_corner(DL)).shift(UP*0.5 + RIGHT*1.25)
+		self.add(stageLabel)
+
+		self.regfile = Rectangle(width=1.4, height=1.2).shift(RIGHT*2 + DOWN*0.4)
+		self.regfileLabel = CodeBlock("regfile", fontSize=30).move_to(self.regfile.get_center())
+		regfileGroup = VGroup(self.regfile, self.regfileLabel)
+
+		self.extractImmval = Rectangle(width=1.5, height=0.75).shift(RIGHT*3.7 + DOWN*0.4)
+		self.extractImmvalLabel = CodeBlock("extract_immval", fontSize=18).move_to(self.extractImmval.get_center())
+		extractImmvalGroup = VGroup(self.extractImmval, self.extractImmvalLabel)
+
+		self.generateControl = Rectangle(width=2, height=0.9).shift(LEFT*1.6 + UP*1.25)
+		self.generateControlLabel = CodeBlock("generate_DMXW_control", fontSize=16).move_to(self.generateControl.get_center())
+		generateControlGroup = VGroup(self.generateControl, self.generateControlLabel)
+
+		self.decideALUOp = Rectangle(width=1.6, height=1).shift(DOWN*0.4 + LEFT*0.35)
+		self.decideALUOpLabel = CodeBlock("decide_ALU_op", fontSize=20).move_to(self.decideALUOp.get_center())
+		decideALUOpGroup = VGroup(self.decideALUOp, self.decideALUOpLabel)
+
+		self.forwardReg = Rectangle(width=1.6, height=1).shift(RIGHT*2 + UP*1.25)
+		self.forwardRegLabel = CodeBlock("forward_reg", fontSize=20).move_to(self.forwardReg.get_center())
+		forwardRegGroup = VGroup(self.forwardReg, self.forwardRegLabel)
+
+		self.dSigs = Rectangle(width=0.7, height=1).move_to(self.generateControl.get_bottom() + DOWN*2)
+		self.dSigsLabel = CodeBlock("D_sigs", fontSize=18).move_to(self.dSigs.get_center())
+		dSigsGroup = VGroup(self.dSigs, self.dSigsLabel)
+
+		self.add(regfileGroup, extractImmvalGroup, generateControlGroup, decideALUOpGroup, forwardRegGroup, dSigsGroup)
+		
+		forwardregBottom = self.forwardReg.get_bottom()
+		regfileLeft = self.regfile.get_left()
+		regfileRight = self.regfile.get_right()
+		regfileTop = self.regfile.get_top()
+		regfileBottom = self.regfile.get_bottom()
+		regfileArrows = [
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileLeft[0], regfileBottom[1]-0.6, 0], end=[regfileLeft[0], regfileBottom[1], 0]),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileLeft[0]+0.3, regfileBottom[1]-0.6, 0], end=[regfileLeft[0]+0.3, regfileBottom[1], 0]),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileLeft[0]+0.6, regfileBottom[1]-0.6, 0], end=[regfileLeft[0]+0.6, regfileBottom[1], 0]),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileLeft[0]+0.9, regfileBottom[1]-0.6, 0], end=[regfileLeft[0]+0.9, regfileBottom[1], 0]),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1,
+				color=RED
+			).put_start_and_end_on(start=[regfileLeft[0]+1.2, regfileBottom[1]-0.6, 0], end=[regfileLeft[0]+1.2, regfileBottom[1], 0]),
+		]
+		self.add(*regfileArrows)
+
+		forwardregArrows = [
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileLeft[0]+0.6, regfileTop[1], 0], end=[regfileLeft[0]+0.6, forwardregBottom[1], 0]),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1
+			).put_start_and_end_on(start=[regfileRight[0]-0.6, regfileTop[1], 0], end=[regfileRight[0]-0.6, forwardregBottom[1], 0]),
+		]
+		self.add(*forwardregArrows)
+
+		dSigsArrows = [
+			Arrow(
+				max_tip_length_to_length_ratio=0.1,
+				color=RED,
+				max_stroke_width_to_length_ratio=2.5
+			).put_start_and_end_on(start=self.generateControl.get_bottom(), end=self.dSigs.get_top()),
+			Arrow(
+				max_tip_length_to_length_ratio=0.1,
+				color=RED
+			).put_start_and_end_on(start=self.dSigs.get_right(), end=self.dSigs.get_right()+RIGHT*0.8)
+		]
+		dSigsLabel = CodeBlock("src2_sel", fontSize=16).next_to(dSigsArrows[1], UP, buff=0.03)
+		self.add(*dSigsArrows, dSigsLabel)
