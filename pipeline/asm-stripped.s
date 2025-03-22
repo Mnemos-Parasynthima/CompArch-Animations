b94f9255401c650d11baab95eef7a37a57e62337
Jorge <jorgegarcia_tx@outlook.com>
2025-03-21 22:46:21 -0500

    Update

--- pipeline/asm-stripped.s
+++ pipeline/asm-stripped.s
@@ -2,7 +2,7 @@ start:
   add x0, x0, #1
   add x1, x1, #0xfff
   nop
-	nop
-	nop
-	add x4, x0, #1
+  nop
+  nop
+  add x4, x0, #1
   ret
--- pipeline/asm.s
+++ pipeline/asm.s
@@ -3,11 +3,11 @@
 	.align 2
 	.p2align 3,,7
 start:
-  add x0, x0, #1
-  add x1, x1, #0xfff
-  nop
+	add x0, x0, #1
+	add x1, x1, #0xfff
+	nop
 	nop
 	nop
 	add x4, x0, #1
-  ret
+	ret
 	.size start, .-start
