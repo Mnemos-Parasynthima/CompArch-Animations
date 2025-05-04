08944fd22d7104cccff8980a479aee65c1135158
Jorge <jorgegarcia_tx@outlook.com>
2025-05-04 00:27:35 -0500

    Update class names

--- caches/main.py
+++ caches/main.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 path.append(str(Path(__file__).resolve().parent.parent))
 
-from Read import Read
-from Write import Write
+from Read import CacheRead
+from Write import CacheWrite
 from Intro import Intro
 
 
@@ -14,7 +14,7 @@ def main():
 
 if __name__ == "__main__":
 	# main()
-	# scene = Intro()
-	scene = Read()
-	scene = Write()
+	scene = Intro()
+	scene = CacheRead()
+	scene = CacheWrite()
 	scene.render()
